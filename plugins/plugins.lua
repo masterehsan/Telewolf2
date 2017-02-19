54fa6ab7bd45a78d0bb8125ee1f399b80cc7bea4
masterehsan <ehsanehsan0831@gmail.com>
2017-02-19 15:25:09 +0330

    TeleWolf

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -22,7 +22,7 @@ local function plugin_exists( name )
 end
 
 local function list_all_plugins(only_enabled)
-  local tmp = check_markdown('\n\n@permag')
+  local tmp = check_markdown('\n\n@TeleWolfTeam')
   local text = ''
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
